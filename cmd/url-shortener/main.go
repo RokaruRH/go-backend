9925f69c25b533e3914416f54eb5c5b75ea26871
RokaruRH <mhergrigur@gmail.com>
2025-04-25 23:56:33 +0400

    added config

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,12 @@
+package main
+import
+
+
+
+func main(){
+	// TODO: init config cleanenv
+	// TODO: init logger slog
+	// TODO: init storage sqlite
+	// TODO: init router chi "chi render"
+	// TODO: run server
+}
