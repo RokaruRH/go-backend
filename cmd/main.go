d51340d70f88abe0ba530a39e14f9758bf3641b1
RokaruRH <mhergrigur@gmail.com>
2025-05-10 13:18:30 +0400

    Pretty Looger and error fix Add pretty logger and adopt standard slog
    - Implement `slogpretty` for colorized local development logging.
    - Migrate from `golang.org/x/exp/slog` to the standard `log/slog`.
    - Enhance HTTP logger: add response bytes/duration, fix log message typo.
    - Suppress unused `storage` variable warning in `main`.

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,8 @@ import (
 	"log/slog"
 	"os"
 	"url-shortener/internal/config"
+	mwLogger "url-shortener/internal/http-server/middleware/logger"
+	"url-shortener/internal/lib/logger/handler/slogpretty"
 	"url-shortener/internal/lib/logger/sl"
 	"url-shortener/internal/storage/sqlite"
 
@@ -29,23 +31,44 @@ func main() {
 		log.Error("failed to init storage", sl.Err(err))
 		os.Exit(1)
 	}
+	_ = storage // Silences the "declared and not used" error for now
 	router := chi.NewRouter()
 	router.Use(middleware.RequestID)
 	router.Use(middleware.Logger)
-	//TODO init router: chi "chi reader"
-
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
 	//TODO init run server
 }
 
 func setuplogger(environment string) *slog.Logger {
 	var log *slog.Logger
+
 	switch environment {
-	case envLocal:
-		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
-	case envDev:
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
-	case envProduction:
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+		case envLocal:
+			log = setupPrettySlog()
+		case envDev:
+			log = slog.New(
+				slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+			)
+		case envProduction:
+			log = slog.New(
+				slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+			)
+		default: // If env config is invalid, set prod settings by default due to security
+			log = slog.New(
+				slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+			)
+		}
+
+		return log
+}
+func setupPrettySlog() *slog.Logger{
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+	 },
 	}
-	return log
+	handler := opts.NewPrettyHandler(os.Stdout)
+ return slog.New(handler)
 }
--- go.mod
+++ go.mod
@@ -7,7 +7,13 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.28
 )
 
-require github.com/go-chi/chi/v5 v5.2.1 // indirect
+require (
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
